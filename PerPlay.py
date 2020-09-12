6d304dfa9d8765ad7966886ea0bd473be3e0d434
ericksonng <ericksonng12@gmail.com>
2020-09-12 13:21:29 -0700

    Code for play by play for live games (or games that have already ended)

---
+++ PerPlay.py
@@ -0,0 +1,17 @@
+import statsapi
+
+sched = statsapi.schedule(date='08/11/2020', team =137)
+gameId = sched[0]["game_id"]
+game_date = sched[0]["game_date"]
+game_result = sched[0]["summary"]
+game = statsapi.get('game', {'gamePk': gameId})
+play = statsapi.get('game_playByPlay', {'gamePk': gameId})
+
+# for i in range(len(play['allPlays'])):
+#     if 'hitData' in play['allPlays'][i]['playEvents'][-1]:
+#         print(i, play['allPlays'][i]['playEvents'][-1]['hitData'])
+
+for i in range(len(game['liveData']['plays']['allPlays'])):
+    if 'hitData' in game['liveData']['plays']['allPlays'][i]['playEvents'][-1]:
+        for e in game['liveData']['plays']['allPlays'][i]['playEvents'][-1]['hitData']:
+            print(i+1,e+":", game['liveData']['plays']['allPlays'][i]['playEvents'][-1]['hitData'][e])
